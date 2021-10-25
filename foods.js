7e9b5443261c19bd128a5f1fe19beb67611776ef
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2021-10-25 14:07:53 -0400

    initial files for draft web page

---
+++ foods.js
@@ -0,0 +1,13 @@
+// const pgraph = document.querySelector('body > div:nth-child(9) > h3:nth-child(2)');
+
+// console.log(pgraph);
+
+// const pgraphs = document.querySelectorAll('div');
+
+/* pgraphs.forEach(pgraph => {
+    console.log(pgraph);
+}); */
+
+const pgraph = document.querySelector('p');
+
+console.log(pgraph.innerText);
